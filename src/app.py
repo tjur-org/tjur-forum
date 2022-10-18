3efbc49be263a2b1689ba0ce1a56153220bda737
Joakim Saario <saario.joakim@gmail.com>
2022-10-18 20:25:37 +0200

    Add tests

--- src/app.py
+++ src/app.py
@@ -1,22 +1,28 @@
-from fastapi import FastAPI
+from pathlib import Path
+
+from fastapi import FastAPI, Request, Response
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+BASE_DIR = Path(__file__).resolve().parent
+
 app = FastAPI()
+app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
-templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
+async def index(request: Request) -> Response:
     return templates.TemplateResponse("index.html", {"request": request})
 
+
 @app.get("/register/", response_class=HTMLResponse)
-async def register_page(request: Request):
+async def register_page(request: Request) -> Response:
     return templates.TemplateResponse("register.html", {"request": request})
 
+
 @app.post("/register/")
-async def register_user():
+async def register_user() -> dict:
     return {}
